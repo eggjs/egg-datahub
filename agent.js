6a3a388850b1929a1c43e29a2b4de77f3cf57898
zivyangll <zivyangll@gmail.com>
2018-04-28 21:46:54 +0800

    modify variable config

--- agent.js
+++ agent.js
@@ -5,11 +5,11 @@ const path = require('path');
 const DataHub = require('macaca-datahub');
 
 module.exports = app => {
-  const datahubConfig = Object.assign({
+  const config = Object.assign({
     store: path.join(app.baseDir, 'data'),
   }, app.config.datahub);
-  _.mkdir(path.resolve(datahubConfig.store));
-  const datahub = new DataHub(datahubConfig);
+  _.mkdir(path.resolve(config.store));
+  const datahub = new DataHub(config);
   datahub.startServer()
     .then(() => console.log('Macaca DataHub started'))
     .catch(e => console.log('Macaca DataHub error', e));
