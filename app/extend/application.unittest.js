efe0cbd5ed77251d9b6d01425bb06edee8176796
zivyangll <zivyangll@gmail.com>
2018-11-08 22:27:02 +0800

    fix: redirect to return DataHub object

--- app/extend/application.unittest.js
+++ app/extend/application.unittest.js
@@ -4,32 +4,6 @@ const DataHubSDK = require('datahub-nodejs-sdk');
 
 module.exports = {
   datahubClient() {
-
-    const datahubClient = new DataHubSDK(this.config.datahub);
-
-    datahubClient.switchScene = async (_options = {}) => {
-
-      if (!_options.pathname) {
-        throw new Error('pathname is required in switchScene');
-      }
-
-      const scene = _options.scene || 'default';
-
-      // update current scene
-      await datahubClient.updateSceneByProjectIdAndDataId(
-        this.config.datahub.hubName,
-        _options.pathname, {
-          currentScene: scene,
-        }
-      );
-
-      return await datahubClient.getSceneDataByProjectIdAndDataId(
-        this.config.datahub.hubName,
-        _options.pathname,
-        scene
-      );
-    };
-
-    return datahubClient;
+    return new DataHubSDK(this.config.datahub);
   },
 };
